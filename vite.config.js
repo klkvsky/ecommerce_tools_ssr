755a227cfb11839e144997732023d8232d86a2e2
Kulikovsky <loke0720@gmail.com>
2022-05-16 06:22:31 +0500

    fixed

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,4 @@
 const vuePlugin = require("@vitejs/plugin-vue");
-const vueJsx = require("@vitejs/plugin-vue-jsx");
 const virtualFile = "@virtual-file";
 const virtualId = "\0" + virtualFile;
 const nestedVirtualFile = "@nested-virtual-file";
