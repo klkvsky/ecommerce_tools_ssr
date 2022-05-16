e9617c96d3f01dc1d5184594c9cd4e610454a1f8
Kulikovsky <loke0720@gmail.com>
2022-05-16 06:17:38 +0500

    423

--- server.js
+++ server.js
@@ -1,18 +1,114 @@
+// // @ts-check
+// const fs = require("fs");
+// const path = require("path");
+// const express = require("express");
+
+// // var http = require("http");
+// // var xml2js = require("xml2js");
+
+// // var parser = new xml2js.Parser();
+// // fs.readFile(__dirname + "/src/assets/test.xml", function (err, data) {
+// //   parser.parseString(data, function (err, result) {
+// //     console.log(result);
+// //     console.log("Done");
+// //   });
+// // });
+
+// const isTest = process.env.NODE_ENV === "test" || !!process.env.VITE_TEST_BUILD;
+
+// async function createServer(
+//   root = process.cwd(),
+//   isProd = process.env.NODE_ENV === "production"
+// ) {
+//   const resolve = (p) => path.resolve(__dirname, p);
+
+//   const indexProd = isProd
+//     ? fs.readFileSync(resolve("dist/client/index.html"), "utf-8")
+//     : "";
+
+//   const manifest = isProd
+//     ? // @ts-ignore
+//       require("./dist/client/ssr-manifest.json")
+//     : {};
+
+//   const app = express();
+
+//   /**
+//    * @type {import('vite').ViteDevServer}
+//    */
+//   let vite;
+//   if (!isProd) {
+//     vite = await require("vite").createServer({
+//       root,
+//       logLevel: isTest ? "error" : "info",
+//       server: {
+//         middlewareMode: "ssr",
+//         watch: {
+//           // During tests we edit the files too fast and sometimes chokidar
+//           // misses change events, so enforce polling for consistency
+//           usePolling: true,
+//           interval: 100,
+//         },
+//       },
+//     });
+//     // use vite's connect instance as middleware
+//     app.use(vite.middlewares);
+//   } else {
+//     app.use(require("compression")());
+//     app.use(
+//       require("serve-static")(resolve("dist/client"), {
+//         index: false,
+//       })
+//     );
+//   }
+
+//   app.use("*", async (req, res) => {
+//     try {
+//       const url = req.originalUrl;
+
+//       let template, render;
+//       if (!isProd) {
+//         // always read fresh template in dev
+//         template = fs.readFileSync(resolve("index.html"), "utf-8");
+//         template = await vite.transformIndexHtml(url, template);
+//         render = require("../dist/server/entry-server.js").render;
+//       } else {
+//         template = indexProd;
+//         render = require("./src/entry-server.js").render;
+//       }
+
+//       const [appHtml, preloadLinks] = await render(url, manifest);
+
+//       const html = template
+//         .replace(`<!--preload-links-->`, preloadLinks)
+//         .replace(`<!--app-html-->`, appHtml);
+
+//       res.status(200).set({ "Content-Type": "text/html" }).end(html);
+//     } catch (e) {
+//       vite && vite.ssrFixStacktrace(e);
+//       console.log(e.stack);
+//       res.status(500).end(e.stack);
+//     }
+//   });
+
+//   return { app, vite };
+// }
+
+// if (!isTest) {
+//   createServer().then(({ app }) =>
+//     app.listen(3000, () => {
+//       console.log("http://localhost:3000");
+//     })
+//   );
+// }
+
+// // for test use
+// exports.createServer = createServer;
+
 // @ts-check
 const fs = require("fs");
 const path = require("path");
 const express = require("express");
-
-// var http = require("http");
-// var xml2js = require("xml2js");
-
-// var parser = new xml2js.Parser();
-// fs.readFile(__dirname + "/src/assets/test.xml", function (err, data) {
-//   parser.parseString(data, function (err, result) {
-//     console.log(result);
-//     console.log("Done");
-//   });
-// });
 
 const isTest = process.env.NODE_ENV === "test" || !!process.env.VITE_TEST_BUILD;
 
@@ -71,10 +167,11 @@ async function createServer(
         // always read fresh template in dev
         template = fs.readFileSync(resolve("index.html"), "utf-8");
         template = await vite.transformIndexHtml(url, template);
-        render = require("../dist/server/entry-server.js").render;
+        render = (await vite.ssrLoadModule("/src/entry-server.js")).render;
       } else {
         template = indexProd;
-        render = require("./src/entry-server.js").render;
+        // @ts-ignore
+        render = require("./dist/server/entry-server.js").render;
       }
 
       const [appHtml, preloadLinks] = await render(url, manifest);
@@ -96,8 +193,8 @@ async function createServer(
 
 if (!isTest) {
   createServer().then(({ app }) =>
-    app.listen(3000, () => {
-      console.log("http://localhost:3000");
+    app.listen(5173, () => {
+      console.log("http://localhost:5173");
     })
   );
 }
